0563b9394f4f9d7a4cf93bcfb0df81c5acb3cb0e
gastonfalena <gastonfalena@gmail.com>
2024-07-02 16:25:13 -0300

    feat(lib): add test mutation with stryker

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig, mergeConfig } from 'vitest/config'
 
 import viteConfig from './vite.config'
@@ -6,6 +7,9 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      globals: true
+      globals: true,
+      alias: {
+        '@src': path.resolve(__dirname, '/src')
+      }
     }
   }))
